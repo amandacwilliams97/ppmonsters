f42204c6ecde6a928c962bf9cd14163847c9be48
Amanda Williams <amandacwilliams97@gmail.com>
2019-02-08 13:58:51 -0500

    fixed

--- index.php
+++ index.php
@@ -9,21 +9,39 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-$monster1 = new Monster("BOO");
-$monster1->attack();
+#require autoload
+require_once ('vendor/autoload.php');
 
-$vampire = new Vampire();
-echo $vampire->getVictims();
-$vampire->attack();
-echo $vampire->getVictims();
-$vampire->attack();
-echo $vampire->getVictims();
+#create an instance of the Base class
+$f3 = Base::instance();
 
-echo "<br><br>";
+#Turn on Fat-Free error reporting
+$f3->set('DEBUG', 3);
 
-$slime = new Slime();
-echo $slime->getSpew();
-$slime->attack();
-echo $slime->getSpew();
-$slime->attack();
-echo $slime->getSpew();
+
+#----------------------------------------------------------------
+#define a default route
+$f3->route('GET /', function() {
+
+    $monster1 = new Monster("BOO");
+    $monster1->attack();
+
+    $vampire = new Vampire();
+    echo $vampire->getVictims();
+    $vampire->attack();
+    echo $vampire->getVictims();
+    $vampire->attack();
+    echo $vampire->getVictims();
+
+    echo "<br><br>";
+
+    $slime = new Slime();
+    echo $slime->getSpew();
+    $slime->attack();
+    echo $slime->getSpew();
+    $slime->attack();
+    echo $slime->getSpew();
+});
+
+#run fat free
+$f3->run();
